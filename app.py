1466220883bb42047b021a9007edd40e479b3ca1
Mahmoud Abo Elfotouh <67564351+MAboElFottouh@users.noreply.github.com>
2022-08-06 18:49:32 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify, render_template, redirect, url_for, f
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('E:/Downloads/Flask/model.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 
 @app.route('/')
